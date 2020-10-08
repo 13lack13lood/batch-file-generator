28d301a21ebc436e73ffbdc533848f0bb37de761
13lack13lood <44007891+13lack13lood@users.noreply.github.com>
2020-10-08 19:43:11 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,14 +7,16 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("(enter changeBatchName (not case senstive) to change the name of the batch file)\nEnter java file name (include .java):");
+		System.out.println("(Enter changeBatchName (not case senstive) to change the name of the batch file)\nEnter java file name (include .java):");
 		String name = scanner.next();
-		String batch = "run " + name.replace(".java", "").replaceAll("[^a-zA-Z0-9]", "") + ".bat";
+		String batch = "run " + name.replace(".java", "").replaceAll(".", "") + ".bat";
 		
 		if(name.toLowerCase().contentEquals("changebatchname")) {
 			System.out.println("Enter new batch file name (you can include spaces and don't include .bat):");
+			
 			scanner.nextLine();
 			batch = scanner.nextLine() + ".bat";
+			
 			System.out.println("Enter name of java file (include .java)");
 			name = scanner.next();
 		}
@@ -24,21 +26,21 @@ public class Main {
 				File file = new File(batch);
 				
 				if (file.createNewFile()) {
-			        System.out.println("File created: " + file.getName());
-			        break;
-			    } else {
-			    	System.out.print("File already exists. Re-run the program.");
-			    	System.exit(0);
-			    }
+			        	System.out.println("File created: " + file.getName());
+			        	break;
+			    	} else {
+			    		System.out.print("File already exists. Re-run the program.");
+			    		System.exit(0);
+			   	}
 			}
 		} catch (Exception e) {
-			System.out.print("\nsomething broke\nhere is the error\n");
+			System.out.print("\nSomething broke\nHere is the error:\n");
 			e.printStackTrace();
 			System.exit(0);
 		}
 		
 		try {
-			System.out.println("writing file");
+			System.out.println("Attempting to write file...");
 			
 			FileWriter writer = new FileWriter(batch);
 			writer.write("@echo off"
@@ -47,17 +49,20 @@ public class Main {
 					+ "\npause");
 			writer.close();
 		} catch (Exception e) {
-			System.out.print("you probably didn't put .java at the end\nhere is the error "
+			System.out.print("You probably didn't put .java at the end\nHere is the error:"
 					+ "\n(if any of this is cut off that means that more stuff probably broke)\n\n");
+			
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
+			
 			e.printStackTrace();
 			System.exit(0);
 		}
-		System.out.println("done");
+		
+		System.out.println("Done!");
 		
 		scanner.close();
 	}
