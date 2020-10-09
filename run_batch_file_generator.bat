fd43981dab4d161af88d401a81b74876f013ce5f
13lack13lood <44007891+13lack13lood@users.noreply.github.com>
2020-10-09 16:35:26 -0400

    Update run_batch_file_generator.bat

--- run_batch_file_generator.bat
+++ run_batch_file_generator.bat
@@ -1,9 +1,9 @@
 @echo off
-if not exist "Main.class" (
-  javac Main.java
+if not exist "Batch_File_Generator.class" (
+  javac Batch_File_Generator.java
 )
 
-java Main
+java Batch_File_Generator
 echo;
 echo Closing in 3 seconds...
 timeout /t 3 > nul
